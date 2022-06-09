673c463ff1161a7997a88bee249fc875883b76df
aparecido nepomuceno <isacnepomuceno.np@gmail.com>
2022-06-09 12:50:00 -0300

    JPQL

--- src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.devsuperior.uri2621.dto.ProductMinDTO;
 import com.devsuperior.uri2621.entities.Product;
 import com.devsuperior.uri2621.projections.ProductMinProjection;
 
@@ -17,4 +18,9 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 			+ "AND providers.name LIKE CONCAT(:beginName, '%')")
 	List<ProductMinProjection> search1(Integer min, Integer max, String beginName);
 	
+	@Query("SELECT new com.devsuperior.uri2621.dto.ProductMinDTO(obj.name) "
+			+ "FROM Product obj "
+			+ "WHERE obj.amount BETWEEN :min AND :max "
+			+ "AND obj.provider.name LIKE CONCAT(:beginName, '%')")
+	List<ProductMinDTO> search2(Integer min, Integer max, String beginName);
 }
